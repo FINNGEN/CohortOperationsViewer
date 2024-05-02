80299acf592ba2ef6f82d62116388a498a0b14b3
Harri Siirtola <harri.siirtola@gmail.com>
2024-05-02 07:57:38 +0300

    - codeWAStable as DT::datatable

--- R/mod_CodeWASPlot.R
+++ R/mod_CodeWASPlot.R
@@ -6,11 +6,10 @@ mod_codeWASPlot_ui <- function(id) {
   ns <- shiny::NS(id)
 
   htmltools::tagList(
-    reactable::reactableOutput(ns("tmp_codeWAStable"))
+    DT::dataTableOutput(ns("codeWAStable")),
   )
 
 }
-
 
 #
 # server
@@ -22,14 +21,87 @@ mod_codeWASPlot_server <- function(id, analysisResultsHandler) {
 
     atlasUrl <- shiny::getShinyOption("cohortOperationsConfig")$atlasUrl
 
+    rowCallback <- c(
+      "function(row, data){",
+      "  for(var i=0; i<data.length; i++){",
+      "    if(data[i] === null){",
+      "      $('td:eq('+i+')', row).html('NA')",
+      "        .css({'color': 'rgb(226,44,41)', 'font-style': 'italic'});",
+      "    }",
+      "  }",
+      "}"
+    )
 
-    output$tmp_codeWAStable <- reactable::renderReactable({
+    output$codeWAStable <- DT::renderDataTable({
+
+      # https://github.com/rstudio/DT/issues/1127
+      # the bug can be worked around by setting shiny.json.digits to a smaller value
+      options(shiny.json.digits = 4)
+
       codeWASData <- analysisResultsHandler$tbl('codewas_results') |>
         dplyr::left_join(analysisResultsHandler$tbl('covariate_ref'), by = c('covariate_id' = 'covariate_id'))  |>
         dplyr::left_join(analysisResultsHandler$tbl('analysis_ref'), by = c('analysis_id' = 'analysis_id')) |>
+        dplyr:::select(-c('is_binary', 'missing_means_zero')) |>
+        dplyr::arrange(p_value) |>
         tibble::as_tibble()
-      reactable::reactable(codeWASData)
+
+      DT::datatable(
+        codeWASData |>
+          dplyr::mutate(p_value = as.numeric(formatC(p_value, format = "e", digits = 2))) |>
+          dplyr::mutate(odds_ratio = as.numeric(formatC(odds_ratio, format = "e", digits = 2))) |>
+          dplyr::mutate(beta = as.numeric(formatC(beta, format = "e", digits = 2))) |>
+          dplyr::mutate(standard_error = as.numeric(formatC(standard_error, format = "e", digits = 2))),
+        colnames = c(
+          'DB id' = 'database_id',
+          'Cov. ID' = 'covariate_id',
+          'N tot' = 'n_total',
+          'N case' = 'n_cases',
+          'N ctrl' = 'n_controls',
+          'p' = 'p_value',
+          'OR' = 'odds_ratio',
+          'Beta' = 'beta',
+          'SE' = 'standard_error',
+          'Model' = 'model_type',
+          'Notes' = 'run_notes',
+          'Cov. name' = 'covariate_name',
+          'Analysis ID' = 'analysis_id',
+          'Concept ID' = 'concept_id',
+          'Analysis name' = 'analysis_name',
+          'Domain' = 'domain_id' #,
+          # 'Binary' = 'is_binary',
+          # 'Missing mean zero' = 'missing_means_zero'
+        ),
+        filter = list(position = 'top', clear = FALSE),
+        options = list(
+          rowCallback = htmlwidgets::JS(rowCallback),
+          autoWidth = TRUE,
+          order = list(list(6, 'asc')),
+          scrollX = TRUE,
+          columnDefs = list(
+            list(width = '45px', targets = c(1:5,7,8,9,10,13,14)),
+            list(width = '50px', targets = c(6))
+          ),
+          pageLength = 10,
+          lengthMenu = c(10, 15, 20, 25, 50)
+        )
+      )
     })
+
+    output$codeWASPlot <- shiny::renderPlot({
+      codeWASData <- analysisResultsHandler$tbl('codewas_results') |>
+        dplyr::left_join(analysisResultsHandler$tbl('covariate_ref'), by = c('covariate_id' = 'covariate_id'))  |>
+        dplyr::left_join(analysisResultsHandler$tbl('analysis_ref'), by = c('analysis_id' = 'analysis_id')) |>
+        dplyr:::select(-c('is_binary', 'missing_means_zero')) |>
+        dplyr::arrange(p_value, covariate_name, analysis_name) |>
+        dplyr::select(p_value, covariate_name, analysis_name) |>
+        tibble::as_tibble()
+
+      codeWASData <- codeWASData |>
+        dplyr::mutate(analysis_num = as.numeric(base::factor(analysis_name))) |>
+        dplyr::mutate(covariate_name = as.numeric(base::factor(covariate_name))) |>
+        tidyr::drop_na()
+    })
+
 
   })
 }
--- tests/testmanual/test-mod_TimeCodeWASVisualisation.R
+++ tests/testmanual/test-mod_TimeCodeWASVisualisation.R
@@ -2,7 +2,7 @@
 # load test results
 devtools::load_all(".")
 
-pathToZip <- testthat::test_path("testdata", "analysisResultsTimeCodeWASSqlite.zip")
+pathToZip <- testthat::test_path("testdata", "codeWAS/eunomia_asthmaVSasthmaControls_CodeWASSqlite.zip")
 
 analysisResultsHandler <- .zipToConnectionHandled(pathToZip)
 
