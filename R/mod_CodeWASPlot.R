7e68735a35d38b301c7aad56b28439e5b70bbca2
Harri Siirtola <harri.siirtola@gmail.com>
2024-05-10 09:05:51 +0300

    - n total on top of the table - order of columns

--- R/mod_CodeWASPlot.R
+++ R/mod_CodeWASPlot.R
@@ -6,7 +6,15 @@ mod_codeWASPlot_ui <- function(id) {
   ns <- shiny::NS(id)
 
   htmltools::tagList(
+    htmltools::h4("CodeWAS Results"),
+    shiny::div(
+      style = "margin-top: 10px; margin-bottom: 20px;",
+      # shiny::tags$em("CodeWAS results are displayed below. The table is sorted by p-value in ascending order."),
+      shiny::textOutput(ns("total_n")),
+
+    ),
     DT::dataTableOutput(ns("codeWAStable")),
+    shiny::downloadButton(ns("downloadCodeWAS"), "Download CodeWAS results", icon = icon("download"))
   )
 
 }
@@ -21,6 +29,11 @@ mod_codeWASPlot_server <- function(id, analysisResultsHandler) {
 
     atlasUrl <- shiny::getShinyOption("cohortOperationsConfig")$atlasUrl
 
+    output$total_n <- shiny::renderText({
+      shiny::req(r$codeWASData)
+      paste("Total N: ", dplyr::first(r$codeWASData$n_total))
+    })
+
     rowCallback <- c(
       "function(row, data){",
       "  for(var i=0; i<data.length; i++){",
@@ -32,29 +45,42 @@ mod_codeWASPlot_server <- function(id, analysisResultsHandler) {
       "}"
     )
 
+    # reactive values
+    r <- shiny::reactiveValues(
+      codeWASData = NULL
+    )
+
+    shiny::observe({
+      r$codeWASData <- analysisResultsHandler$tbl('codewas_results') |>
+        dplyr::left_join(analysisResultsHandler$tbl('covariate_ref'), by = c('covariate_id' = 'covariate_id'))  |>
+        dplyr::left_join(analysisResultsHandler$tbl('analysis_ref'), by = c('analysis_id' = 'analysis_id')) |>
+        dplyr::mutate(odds_ratio = ifelse(is.na(odds_ratio), exp(beta), odds_ratio)) |>
+        dplyr:::select(-c('is_binary', 'missing_means_zero')) |>
+        tibble::as_tibble()
+    })
+
     output$codeWAStable <- DT::renderDataTable({
+      req(r$codeWASData)
 
       # https://github.com/rstudio/DT/issues/1127
       # the bug can be worked around by setting shiny.json.digits to a smaller value
       options(shiny.json.digits = 4)
 
-      codeWASData <- analysisResultsHandler$tbl('codewas_results') |>
-        dplyr::left_join(analysisResultsHandler$tbl('covariate_ref'), by = c('covariate_id' = 'covariate_id'))  |>
-        dplyr::left_join(analysisResultsHandler$tbl('analysis_ref'), by = c('analysis_id' = 'analysis_id')) |>
-        dplyr:::select(-c('is_binary', 'missing_means_zero')) |>
-        dplyr::arrange(p_value) |>
-        tibble::as_tibble()
-
       DT::datatable(
-        codeWASData |>
+        r$codeWASData |>
           dplyr::mutate(p_value = as.numeric(formatC(p_value, format = "e", digits = 2))) |>
           dplyr::mutate(odds_ratio = as.numeric(formatC(odds_ratio, format = "e", digits = 2))) |>
           dplyr::mutate(beta = as.numeric(formatC(beta, format = "e", digits = 2))) |>
-          dplyr::mutate(standard_error = as.numeric(formatC(standard_error, format = "e", digits = 2))),
+          dplyr::mutate(standard_error = as.numeric(formatC(standard_error, format = "e", digits = 2))) |>
+          dplyr::select(-n_total),
         colnames = c(
-          'DB id' = 'database_id',
+          'Database' = 'database_id',
+          'Domain' = 'domain_id',
+          'Analysis' = 'analysis_name',
+          'Name' = 'covariate_name',
+          'Concept ID' = 'concept_id',
           'Cov. ID' = 'covariate_id',
-          'N tot' = 'n_total',
+          # 'N tot' = 'n_total',
           'N case' = 'n_cases',
           'N ctrl' = 'n_controls',
           'p' = 'p_value',
@@ -63,11 +89,7 @@ mod_codeWASPlot_server <- function(id, analysisResultsHandler) {
           'SE' = 'standard_error',
           'Model' = 'model_type',
           'Notes' = 'run_notes',
-          'Cov. name' = 'covariate_name',
           'Analysis ID' = 'analysis_id',
-          'Concept ID' = 'concept_id',
-          'Analysis name' = 'analysis_name',
-          'Domain' = 'domain_id' #,
           # 'Binary' = 'is_binary',
           # 'Missing mean zero' = 'missing_means_zero'
         ),
@@ -75,11 +97,11 @@ mod_codeWASPlot_server <- function(id, analysisResultsHandler) {
         options = list(
           rowCallback = htmlwidgets::JS(rowCallback),
           autoWidth = TRUE,
-          order = list(list(6, 'asc')),
+          order = list(list(5, 'asc')), # p_value
           scrollX = TRUE,
           columnDefs = list(
-            list(width = '45px', targets = c(1:5,7,8,9,10,13,14)),
-            list(width = '50px', targets = c(6))
+            list(width = '45px', targets = c(1:4,7,8,9,10,12,13)),
+            list(width = '50px', targets = c(5, 6)) # p_value, OR
           ),
           pageLength = 10,
           lengthMenu = c(10, 15, 20, 25, 50)
@@ -87,21 +109,14 @@ mod_codeWASPlot_server <- function(id, analysisResultsHandler) {
       )
     })
 
-    output$codeWASPlot <- shiny::renderPlot({
-      codeWASData <- analysisResultsHandler$tbl('codewas_results') |>
-        dplyr::left_join(analysisResultsHandler$tbl('covariate_ref'), by = c('covariate_id' = 'covariate_id'))  |>
-        dplyr::left_join(analysisResultsHandler$tbl('analysis_ref'), by = c('analysis_id' = 'analysis_id')) |>
-        dplyr:::select(-c('is_binary', 'missing_means_zero')) |>
-        dplyr::arrange(p_value, covariate_name, analysis_name) |>
-        dplyr::select(p_value, covariate_name, analysis_name) |>
-        tibble::as_tibble()
-
-      codeWASData <- codeWASData |>
-        dplyr::mutate(analysis_num = as.numeric(base::factor(analysis_name))) |>
-        dplyr::mutate(covariate_name = as.numeric(base::factor(covariate_name))) |>
-        tidyr::drop_na()
-    })
-
+    output$downloadCodeWAS <- downloadHandler(
+      filename = function() {
+        paste(Sys.time(), '_result.csv', sep='')
+      },
+      content = function(file) {
+        write.csv(r$codeWASData, file, row.names = FALSE)
+      }
+    )
 
   })
 }
